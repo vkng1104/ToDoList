3caaf1b66a6093dc2df7081d2331bc70d16de189
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-10-10 23:51:10 +0700

    Activity model

---
+++ model/activity.js
@@ -0,0 +1,10 @@
+class Activity {
+  constructor(name, date, dateStr, finished) {
+    this.name = name;
+    this.date = date;
+    this.dateStr = dateStr;
+    this.finished = finished;
+  }
+}
+
+export { Activity };
